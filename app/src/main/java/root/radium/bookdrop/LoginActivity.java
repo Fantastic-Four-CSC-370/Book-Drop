0ad00efe5c70c25da815989683dddce28e7162d2
Mohammad Hasibur Rahman Shuvo <18103361@iubat.edu>
2021-02-01 22:39:51 +0600

    Login Activity added

---
+++ app/src/main/java/root/radium/bookdrop/LoginActivity.java
@@ -0,0 +1,88 @@
+package root.radium.bookdrop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    final String TAG = "loginForm";
+    Button lbtn;
+    EditText mid, mpass;
+    TextView mcreatenew;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Firebase Auth;
+        auth = FirebaseAuth.getInstance();
+
+        lbtn = (Button) findViewById(R.id.lbtn);
+        mid = (EditText) findViewById(R.id.sid);
+        mpass = (EditText) findViewById(R.id.spass);
+        mcreatenew = (TextView) findViewById(R.id.createnew);
+        lbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String LoginEmail = mid.getText().toString().trim();
+                String LoginPass = mpass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(LoginEmail)) {
+                    Toast.makeText(LoginActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(LoginPass)) {
+                    Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (LoginPass.length() < 6) {
+                    Toast.makeText(LoginActivity.this, "Too short Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                auth.signInWithEmailAndPassword(LoginEmail, LoginPass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Login Failed!! \n Try again", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        mcreatenew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+
+    }
+}
