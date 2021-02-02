119b0e71425232efe05b33dc48bee1557f39479f
Hasibur Rahman Shuvo <57192512+mhrshuvo@users.noreply.github.com>
2021-02-03 00:43:30 +0600

    signup activity added. backend not ok

---
+++ app/src/main/java/root/radium/bookdrop/signupForm.java
@@ -0,0 +1,14 @@
+package root.radium.bookdrop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class signupForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_form);
+    }
+}
