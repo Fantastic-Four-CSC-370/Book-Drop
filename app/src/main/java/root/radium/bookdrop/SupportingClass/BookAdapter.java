9eaa41b66cbc9f9b489c51f2d1bb1c704d57cc33
Mohammad Hasibur Rahman Shuvo <18103361@iubat.edu>
2021-02-15 11:27:30 +0600

    REV 15*02*21

--- app/src/main/java/root/radium/bookdrop/SupportingClass/BookAdapter.java
+++ app/src/main/java/root/radium/bookdrop/SupportingClass/BookAdapter.java
@@ -7,19 +7,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.android.volley.Response;
 import com.squareup.picasso.Picasso;
-
 import org.json.JSONObject;
-
 import java.util.List;
-
 import root.radium.bookdrop.R;
-import root.radium.bookdrop.recyclerview.FoodModel;
+
 
 public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
 
